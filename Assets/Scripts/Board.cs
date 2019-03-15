2728573e56284f75185cd6755956076c9a826a8a
Hugo-C <hchastel@laposte.net>
2019-03-15 23:32:43 +0100

    Added 3D visualisation

--- Assets/Scripts/Board.cs
+++ Assets/Scripts/Board.cs
@@ -1,11 +1,15 @@
 ﻿using System;
 using System.Collections;
+using System.Collections.Generic;
 using System.ComponentModel;
 using DefaultNamespace;
 using UnityEngine;
 using Random = UnityEngine.Random;
 
 public class Board : MonoBehaviour {
+    
+    public GameObject terrain;
+    
     public GameObject emptyCellPrefab;
     public GameObject treeCellPrefab;
     public GameObject burningCellPrefab;
@@ -22,14 +26,15 @@ public class Board : MonoBehaviour {
     [Range(0.01f, 10f)]
     public float secondsBetweenSteps;
 
-    public bool isRunning;
+    private bool isRunning;
     
     private Cell[,] map;
     private GameObject[,] gameObjectMap;
 
-    private void Start() {
+    private void Awake() {
         map = new Cell[width, height];
         gameObjectMap = new GameObject[width, height];
+        InitTerrain();
         for (int x = 0; x <= map.GetUpperBound(0); x++) {
             for (int y = 0; y <= map.GetUpperBound(1); y++) {
                 map[x, y] = Cell.Empty;
@@ -45,11 +50,29 @@ public class Board : MonoBehaviour {
     private IEnumerator RunningAutomata() {
         yield return new WaitForSeconds(secondsBetweenSteps);
         int stepCount = 0;
-        while (isRunning) {
-            map = Step();
-            stepCount++;
-            Debug.Log("Step : " + stepCount);
+        while (true) {
+            if (isRunning) {
+                map = Step();
+                stepCount++;
+                Debug.Log("Step : " + stepCount);
+            }
             yield return new WaitForSeconds(secondsBetweenSteps);
+        }
+    }
+
+    [ContextMenu("TogglePause")]
+    private void TogglePause() {
+        isRunning = !isRunning;
+    }
+    
+    /// <summary>
+    /// Init a terrain with the terrain prefab
+    /// </summary>
+    private void InitTerrain() {
+        if (terrain != null) {
+            // TODO improve terrain gameobject pivot point
+            var goTerrain = Instantiate(terrain, new Vector3(width * 2f, 0, height * 2f), terrain.transform.rotation);
+            goTerrain.transform.localScale = new Vector3(width / 4.9f, 1f, height / 4.9f);
         }
     }
 
@@ -75,7 +98,9 @@ public class Board : MonoBehaviour {
                 break;
         }
 
-        var go = Instantiate(prefabToInstantiate, new Vector3(x, y), Quaternion.identity);
+        if (prefabToInstantiate == null)
+            return;
+        var go = Instantiate(prefabToInstantiate, new Vector3(x * 2, 0, y * 2), prefabToInstantiate.transform.rotation);
         gameObjectMap[x, y] = go;
     }
 
@@ -127,5 +152,28 @@ public class Board : MonoBehaviour {
             }
         }
         return false;
+    }
+
+    public Transform GetCenterTransform() {
+        var go = new GameObject("center");
+        go.transform.position = new Vector3(width, 0, height);
+        return go.transform;
+    }
+    
+    public List<Transform> GetCornersTransforms() {
+        var res = new List<Transform>();
+        var go1 = new GameObject("corner1");
+        go1.transform.position = new Vector3(0, 0, 0);
+        res.Add(go1.transform);
+        var go2 = new GameObject("corner2");
+        go2.transform.position = new Vector3(width * 2, 0, 0);
+        res.Add(go2.transform);
+        var go3 = new GameObject("corner3");
+        go3.transform.position = new Vector3(width * 2, 0, height * 2);
+        res.Add(go3.transform);
+        var go4 = new GameObject("corner4");
+        go4.transform.position = new Vector3(0, 0, height * 2);
+        res.Add(go4.transform);
+        return res;
     }
 }
---
+++ Assets/Scripts/MyCamera.cs
@@ -0,0 +1,51 @@
+﻿using System.Collections.Generic;
+using UnityEngine;
+
+public class MyCamera : MonoBehaviour {
+    public GameObject boardGo;
+
+    [Range(0f, 0.5f)]
+    public float canSeeMargin;
+    
+    [Range(1f, 100f)]
+    public float speed;
+
+
+    private Transform center;
+    private List<Transform> corners;
+    private Camera cameraComponent;
+
+    private void Start() {
+        Board board = boardGo.GetComponent<Board>();
+        center = board.GetCenterTransform();
+        corners = board.GetCornersTransforms();
+        cameraComponent = gameObject.GetComponent<Camera>();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        transform.LookAt(center);
+        transform.Translate(Vector3.right * Time.deltaTime * speed);
+        bool canSeeItAll = true;
+        foreach (var corner in corners) {
+            if (!CanSee(corner)) {
+                canSeeItAll = false;
+                break;
+            }
+        }
+
+        if (!canSeeItAll)
+            cameraComponent.orthographicSize += 0.1f;
+    }
+
+    /// <summary>
+    /// Indicates if the camera can see the given transform
+    /// </summary>
+    /// <param name="t"></param>
+    /// <returns></returns>
+    public bool CanSee(Transform t) {
+        Vector3 viewportPoint = cameraComponent.WorldToViewportPoint(t.position);
+        return viewportPoint.x - canSeeMargin > 0 && viewportPoint.x + canSeeMargin < 1 && 
+               viewportPoint.y - canSeeMargin > 0 && viewportPoint.y + canSeeMargin < 1;
+    }
+}
