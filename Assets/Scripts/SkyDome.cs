f2380db8fac0e786045f9c1de5b5fbfd379acead
Hugo <hchastel@laposte.net>
2019-11-17 11:26:09 +0100

    Added skydom offset shift

---
+++ Assets/Scripts/SkyDome.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SkyDome : MonoBehaviour {
+    
+    public float offsetSpeed = 0.001f;
+
+    private Renderer rend;
+    // Start is called before the first frame update
+    void Start() {
+        rend = GetComponent<Renderer> ();
+    }
+
+    // Update is called once per frame
+    void Update() {
+        var material = rend.material;
+        var offset = material.mainTextureOffset;
+        offset += offsetSpeed * Time.deltaTime * Vector2.right;
+        material.mainTextureOffset = offset;
+    }
+}
